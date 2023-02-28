8e34826b31d9495b02b0cc3a59ab7f1ca997296a
Korkrane <baudoin.haas@gmail.com>
2023-02-28 18:55:20 +0100

    docs(swagger): defines new endpoint for movies/comments/users

--- server/swagger.js
+++ server/swagger.js
@@ -14,7 +14,65 @@ export default {
         summary: 'Returns list of all users',
         description: 'Returns a list of users with their id and their username',
         parameters: [],
-        responses: {},
+        responses: {
+          200: {
+            description: 'Successful operation',
+            schema: {
+              type: 'array',
+              items: {
+                $ref: '#/definitions/User',
+              },
+            },
+          },
+          400: {
+            description: 'Invalid request',
+          },
+          404: {
+            description: 'Element not found',
+          },
+        },
+      },
+      post: {
+        tags: ['users'],
+        summary: 'Create a new user',
+        description: '',
+        parameters: [
+          {
+            name: 'firstName',
+            in: 'formData',
+            required: true,
+            type: 'string',
+            default: 'John',
+          },
+          {
+            name: 'lastName',
+            in: 'formData',
+            required: true,
+            type: 'string',
+            default: 'Doe',
+          },
+          {
+            name: 'email',
+            in: 'formData',
+            required: true,
+            type: 'string',
+            default: 'john.doe@gmail.com',
+          },
+        ],
+        responses: {
+          201: {
+            description: 'Successful operation',
+            schema: {
+              $ref: '#/definitions/User',
+            },
+          },
+          400: {
+            description: 'Invalid request',
+          },
+          404: {
+            description: 'Element not found',
+          },
+        },
       },
     },
     '/users/{id}': {
@@ -47,7 +105,12 @@ export default {
           },
         },
       },
+      patch: {
+        tags: ['users'],
+        summary: 'Update details of a specific user [TO COMPLETE]',
+      },
     },
+
     '/auth/facebook': {
       get: {
         tags: ['auth'],
@@ -100,6 +163,122 @@ export default {
         },
       },
     },
+    '/movies': {
+      get: {
+        tags: ['movies'],
+        summary: 'Returns list of all movies',
+        description: 'Authenticate a user using Github.',
+        parameters: [],
+        responses: {
+          200: {
+            description: 'Successful operation',
+            schema: {
+              type: 'array',
+              items: {
+                $ref: '#/definitions/Movies',
+              },
+            },
+          },
+          400: {
+            description: 'Invalid request',
+          },
+          404: {
+            description: 'Element not found',
+          },
+        },
+      },
+    },
+    '/movies/{id}': {
+      get: {
+        tags: ['movies'],
+        summary: 'Rerturns details of a specific movie',
+        description: 'Authenticate a user using Github.',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            required: true,
+            type: 'integer',
+          },
+        ],
+        responses: {
+          200: {
+            description: 'Successful operation',
+            schema: {
+              $ref: '#/definitions/Movies',
+            },
+          },
+          400: {
+            description: 'Invalid request',
+          },
+          404: {
+            description: 'Element not found',
+          },
+        },
+      },
+    },
+    '/comments': {
+      post: {
+        tags: ['comments'],
+        summary: 'Add a comment about a specific movie',
+        description: '',
+        parameters: [
+          {
+            name: 'content',
+            in: 'formData',
+            required: true,
+            type: 'string',
+          },
+        ],
+        responses: {
+          201: {
+            description: 'Successful operation',
+            schema: {
+              $ref: '#/definitions/Comments',
+            },
+          },
+          400: {
+            description: 'Invalid request',
+          },
+          404: {
+            description: 'Element not found',
+          },
+        },
+      },
+    },
+    '/comments/movies/{id}': {
+      get: {
+        tags: ['comments'],
+        summary: 'Returns all the comment of the movie {id}',
+        description:
+          'Returns a list of all the comments linked to a movie, it should be ordered with the most recents first',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            required: true,
+            type: 'integer',
+          },
+        ],
+        responses: {
+          200: {
+            description: 'Successful operation',
+            schema: {
+              type: 'array',
+              items: {
+                $ref: '#/definitions/Comments',
+              },
+            },
+          },
+          400: {
+            description: 'Invalid request',
+          },
+          404: {
+            description: 'Element not found',
+          },
+        },
+      },
+    },
   },
   definitions: {
     User: {
@@ -117,6 +296,34 @@ export default {
         email: {
           type: 'string',
           example: 'john.doe@gmail.com',
+        },
+      },
+    },
+    Movies: {
+      type: 'object',
+      required: [],
+      properties: {
+        title: {
+          type: 'string',
+          example: 'MovieTitle',
+        },
+      },
+    },
+    Comments: {
+      type: 'object',
+      required: ['authorId', 'movieId'],
+      properties: {
+        authorId: {
+          type: 'integer',
+          example: '1',
+        },
+        movieId: {
+          type: 'integer',
+          example: '1',
+        },
+        content: {
+          type: 'string',
+          example: 'What a movie',
         },
       },
     },
