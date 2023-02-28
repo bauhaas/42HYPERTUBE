99bb141eb8a71bfdb2ead9cfcc574daeb5678b79
Korkrane <baudoin.haas@gmail.com>
2023-02-28 16:29:29 +0100

    refactor(user): use db.users instead of Users  for sequelize function call

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -1,15 +1,13 @@
 import db from '../models/index.js';
 import { NotFoundError } from '#utils/errorHandler';
 
-const Users = db.users;
-
 export const findAll = async () => {
-  const data = await Users.findAll();
+  const data = await db.users.findAll();
   return data;
 };
 
 export const findOne = async (id) => {
-  const data = await Users.findByPk(id);
+  const data = await db.users.findByPk(id);
   if (!data) {
     throw new NotFoundError('User not found');
   }
