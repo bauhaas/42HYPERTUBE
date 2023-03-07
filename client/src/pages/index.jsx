152f8f8dd236ab359e2a5c7961612663167a7dcd
Korkrane <baudoin.haas@gmail.com>
2023-03-07 16:41:08 +0100

    feat(routes): add basic routing system for client pages

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,3 +1,8 @@
 export { Signin } from './Signin/Signin';
+export { Signup } from './Signup/Signup';
+export { Settings } from './Settings/Settings';
+export { User } from './User/User';
+export { Movie } from './Movie/Movie';
+export { Unknown } from './Unknown/Unknown';
 export { Test } from './Test/Test';
 export { Home } from './Home/Home';
